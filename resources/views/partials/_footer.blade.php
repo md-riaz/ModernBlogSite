e6bae302a12594cff82fb49b3e8a08cde35928ba
mdriaz <riazmd582@gmail.com>
2020-04-24 22:42:24 +0600

    partials with include

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>@yield('title')</title>
+
+<link rel="shortcut icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
+<link rel="icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
+<!-- Styles -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('public/css/toastr.css') }}" rel="stylesheet">
